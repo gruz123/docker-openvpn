21f63adbe8127be3e5378409220f4fa9e7703a18
gruz123 <gruz123@gmail.com>
2022-11-15 12:45:00 +0000

    c

--- start.in.sh
+++ start.in.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 ovpn_genconfig -u udp://$(curl -s http://whatismyip.akamai.com/ | cut -b 1-16)
 ovpn_initpki
-echo "Set prefix"
-read CLIENTNAME
-echo "qnty?"
-read qnty
-for (( i=1; i <= $qnty; i++ ))
-do
-echo " $i"
-easyrsa build-client-full $CLIENTNAME$i nopass
-ovpn_getclient $CLIENTNAME$i > $CLIENTNAME$i.ovpn
-done
