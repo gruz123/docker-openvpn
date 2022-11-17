41f0757d3b37477252ea478281444015ba8e30c2
gruz123 <gruz123@gmail.com>
2022-11-17 20:39:57 +0000

    useradd by qnty, name

--- usr.in.sh
+++ usr.in.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
-echo "Set prefix"
+echo "Set prefix(name)"
 read CLIENTNAME
-echo "qnty?"
-read qnty
-for (( i=1; i <= $qnty; i++ ))
+if [ -d "clients/$CLIENTNAME" ]
+    then
+    e=$(ls clients/$CLIENTNAME/ | wc -l) || :
+    else 
+    e=0
+    mkdir clients/$CLIENTNAME
+fi
+echo "quantity? 1-25"
+read q
+if [[ "${q}" =~ ^([0-9]) ]]; then
+   if [[ $q -lt 25 ]]
+   then 
+t=$(($e+$q))
+for (( i=1; i <= $t; i++ ))
 do
-echo " $i"
 easyrsa --passin=pass:$(cat $OPENVPN/psk)  build-client-full $CLIENTNAME$i nopass
-getclient $CLIENTNAME$i > $CLIENTNAME$i.ovpn
+getclient $CLIENTNAME$i > clients/$CLIENTNAME/$CLIENTNAME$i.ovpn
+echo "added $q, total =$t"
 done
+   else 
+      echo "allowed only 25 users per request"
+   fi
+else
+echo "error: Not a number" >&2; exit 1
+fi
