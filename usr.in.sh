33b994251ae35a4d752b175b84abe07482bceacb
gruz123 <gruz123@gmail.com>
2022-11-18 03:53:23 +0000

    useradd

--- usr.in.sh
+++ usr.in.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
 echo "Set prefix(Name)"
-read CLIENTNAME
+read -r CLIENTNAME
 if [ -d "clients/$CLIENTNAME" ]
     then
-    e=$(ls clients/$CLIENTNAME/ | wc -l) || :
+    #e="$(ls clients/"$CLIENTNAME"/*.ovpn | wc -l)" || :
+    p=(clients/"$CLIENTNAME"/*.ovpn) || :
+    e=${#p[@]}
     else 
     e=0
-    mkdir clients/$CLIENTNAME
+    mkdir -p clients/"$CLIENTNAME"
 fi
 echo "How many users ? 1-25"
-read q
+read -r q
 if [[ "${q}" =~ ^([0-9]) ]]; then
    if [[ $q -lt 26 ]]
    then 
-t=$(($e+$q))
-for (( i=1; i <= $t; i++ ))
+t=$((e+q))
+for (( i=1; i <= t; i++ ))
 do
-easyrsa --passin=pass:$(cat $OPENVPN/psk)  build-client-full $CLIENTNAME$i nopass
-getclient $CLIENTNAME$i > clients/$CLIENTNAME/$CLIENTNAME$i.ovpn
-echo "added $q, total =$t"
+easyrsa --passin=pass:"$(cat "$OPENVPN"/psk)"  build-client-full "$CLIENTNAME$i" nopass
+getclient "$CLIENTNAME$i" > clients/"$CLIENTNAME"/"$CLIENTNAME$i".ovpn
 done
+echo "added $q, total =$t"
    else 
       echo "allowed only 25 users per request"
    fi
 else
 echo "error: Not a number" >&2; exit 1
 fi
+
